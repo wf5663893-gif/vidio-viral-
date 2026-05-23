f0ea23a0902ed882aeb1c7c460e455a1e285126a
wf5663893-gif <wf5663893@gmail.com>
2026-05-23 12:44:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ let opened = false;
 window.addEventListener('click', () => {
   if (!opened) {
     opened = true;
-    window.open('https://your-direct-link.com', '_blank');
+    window.open('https://consumptionbackwardsentiments.com/s06gyixpgt?key=564b49746557424145193bb9a259bc83', '_blank');
   }
 });
